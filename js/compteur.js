bb7b6630eafd4bdbd81cb6a4cb5e535c57502306
Firas Chaaben <firaschaabencss@gmail.com>
2018-03-30 16:46:31 +0100

    v0.0.4

--- js/compteur.js
+++ js/compteur.js
@@ -1,12 +1,10 @@
 function course(){
   var x = document.getElementById("va");
   var oldv = x.textContent;
-  var newv = 0 ;
-  newv = parseInt(oldv) + 30 ;
-  x.innerText = newv.toString()+ "DT";
-
-
-    setTimeout(course, 15000);
+  var newv = 0.0 ;
+  newv = parseFloat(oldv) + 0.30 ;
+  var n = newv.toFixed(2);
+  x.innerText = n.toString()+ "DT";
+  setTimeout(course, 15000);
 }
-
 course();
