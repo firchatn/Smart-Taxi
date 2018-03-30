6662a3fdf110105e6c3d47cf2abc9087b1e76c1d
Firas Chaaben <firaschaabencss@gmail.com>
2018-03-30 23:11:21 +0100

    update compteur

--- js/compteur.js
+++ js/compteur.js
@@ -1,10 +1,23 @@
+var s = document.getElementById("start");
+var p = document.getElementById("stop");
+s.addEventListener("click",course);
+p.addEventListener("click",stop);
+var on = true;
+
+function stop(){
+  console.log("stop");
+  on = false;
+}
+
+
 function course(){
   var x = document.getElementById("va");
   var oldv = x.textContent;
   var newv = 0.0 ;
   newv = parseFloat(oldv) + 0.30 ;
   var n = newv.toFixed(2);
+  if (on == true){
   x.innerText = n.toString()+ "DT";
   setTimeout(course, 15000);
+  }
 }
-course();
